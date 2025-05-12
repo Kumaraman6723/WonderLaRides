85118b209a5432805eea167b9475255701c5e948
Aman kumar prasad <kumarprasadaman1234@gmail.com>
2025-05-12 09:50:54 +0530

    uc

--- client/src/components/CarouselControls.jsx
+++ client/src/components/CarouselControls.jsx
@@ -1,20 +1,20 @@
-import React from 'react';
-import { ChevronLeft, ChevronRight } from 'lucide-react';
+import React from "react";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 
 const CarouselControls = ({ onPrevious, onNext }) => {
   return (
     <div className="flex justify-end mb-4">
-      <button 
+      <button
         onClick={onPrevious}
         className="bg-[#FFD700] hover:bg-yellow-500 rounded-full h-10 w-10 flex items-center justify-center mr-2"
       >
-        <ChevronLeft className="h-6 w-6" />
+        <ChevronLeft className="h-6 w-6 text-[#334DCF]" />
       </button>
-      <button 
+      <button
         onClick={onNext}
         className="bg-[#FFD700] hover:bg-yellow-500 rounded-full h-10 w-10 flex items-center justify-center"
       >
-        <ChevronRight className="h-6 w-6" />
+        <ChevronRight className="h-6 w-6 text-[#334DCF]" />
       </button>
     </div>
   );
